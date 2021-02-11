334b602d5c7b1f1357f261a794664f8292b60d0c
Danilock <yo1234re@gmail.com>
2021-02-11 14:47:44 -0400

    Player Movement

---
+++ Sci-Fy/Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.StateMachine;
+
+public class PlayerController : MonoBehaviour, ISetState<PlayerController>
+{
+    #region PlayerStates
+    private IState<PlayerController> _currentState;
+    public PlayerIdleState IdleState = new PlayerIdleState();
+    public PlayerMovingState MovingState = new PlayerMovingState();
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetState(IdleState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentState.TickState(this);
+    }
+
+    public void SetState(IState<PlayerController> newState)
+    {
+        _currentState?.ExitState(this);
+        _currentState = newState;
+        _currentState.EnterState(this);
+    }
+}
